b9fb232247cf252194eaa23434a880ff76b65eb2
ma4723 <ma4723@ic.ac.uk>
2024-02-03 19:40:01 +0000

    Fixed app.js change

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 import Navigator from "./src/navigation/index";
+import HomeScreen from "./src/screens/HomeScreen";
+import TimerScreen from "./src/screens/TimerScreen";
 
 export default function App() {
   return (
     <Navigator />
     // <View style={styles.container}>
-      
+
     //   <Navigator />
 
     //   <StatusBar style="auto" />
